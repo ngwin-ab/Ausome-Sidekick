07c7ecdcc8ee23390454e541b8ac096c5e86a5cf
An Ba <ngwin.ab@gmail.com>
2023-07-24 16:14:02 -0400

    Add Home screen content

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text>Home Screen</Text>
+        <View style={style.container}>
+        <TouchableOpacity style={style.box}>
+            <Ionicons name="add-circle" size={24} color="black" />
+            <Text>Add a child</Text>
+        </TouchableOpacity>
         </View>
     );
 }
@@ -18,4 +22,13 @@ const style = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+
+    box: {
+        width: 200,
+        height: 100,
+        backgroundColor: 'beige',
+        borderWidth: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
