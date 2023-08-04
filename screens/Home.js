c88bcf04e4763bb26d97d5c0cafc1351c2d002bf
An Ba <ngwin.ab@gmail.com>
2023-08-04 14:42:59 -0400

    display kids list with data fetched from API

--- screens/Home.js
+++ screens/Home.js
@@ -1,28 +1,90 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import React, { useState, useEffect } from "react";
+import { ScrollView, SafeAreaView, Button, View, Text, StyleSheet, TouchableOpacity, Image, FlatList } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Separator = () => <View style={styles.separator} />;
+
+const ChildBox = ({ navigation }) => {
+    const [data, setData] = useState([]);
+    
+    const getData = () => {
+        const url = 'http://10.0.0.136:3000/kids';
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(result => {
+            setData(result);
+        })
+        .catch(error => {
+            // Handle any errors that occurred during the fetch or parsing of response
+            console.error('Error fetching data:', error);
+        });
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const [selectedId, setSelectedId] = useState();
+
+    const Item = ({ item, onPress }) => (
+        <TouchableOpacity onPress={onPress} style={styles.item}>
+            <Text>{item.name}</Text>
+        </TouchableOpacity>
+    );
+    
+    const renderItem = ({ item }) => {
+
+        return (
+            <Item
+                item={item}
+                key={item.id}
+                onPress={() => {
+                    setSelectedId(item.id);
+                    navigation.navigate('ChildData', { kidId: item.id });
+                }}
+            />
+        );
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+                extraData={selectedId}
+            />
+        </SafeAreaView>
+    );
+}
 
 const Home = ({ navigation }) => {
+
     return (
         <View style={styles.container}>
-            <TouchableOpacity
+            {/* <TouchableOpacity
                 style={styles.kidbox}
-                onPress={() => navigation.navigate('ChildData')}>
-                <Image style={styles.avatar} source={require('../assets/Icons/Cat_3.png')} />
-                <Text style={styles.heading}>Child's name</Text>
-            </TouchableOpacity>
+                onPress={() => navigation.navigate('ChildData')}> */}
+            {/* <Image style={styles.avatar} source={require('../assets/Icons/Cat_3.png')} /> */}
+            {/* <Text style={styles.heading}>{childName}</Text>
+            </TouchableOpacity> */}
+            <ChildBox navigation={navigation} />
             <Separator />
             <TouchableOpacity
                 style={styles.addbox}
                 onPress={() => navigation.navigate('AddChild')}>
-                <Ionicons name='add-circle' size={90} color='rgb(86, 136, 159)' />
+                <Ionicons name='add-circle' size={50} color='rgb(86, 136, 159)' />
                 <Text style={styles.heading}>Add child</Text>
             </TouchableOpacity>
         </View>
     );
 }
+
+const Separator = () => <View style={styles.separator} />;
 
 export default Home;
 
@@ -30,25 +92,44 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'rgb(235, 243, 250)',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
 
-    kidbox: {
-        borderColor: 'rgb(123, 165, 185)',
-        backgroundColor: 'rgb(167, 200, 215)',
-        borderWidth: 2,
-        flexDirection: 'row',  
-        margin: 10,
-        padding: 10,
-        alignItems: 'center',
-    }, 
+    item: {
+        backgroundColor: 'rgb(196, 216, 228)',
+        borderColor: 'white',
+        borderRadius: 5,
+        padding: 20,
+        width: 300,
+        height: 110,
+        marginVertical: 8,
+        marginHorizontal: 15,
+        alignSelf: "center"
+    },
+    // kidbox: {
+    //     borderColor: 'white',
+    //     borderRadius: 5,
+    //     backgroundColor: 'rgb(196, 216, 228)',
+    //     borderWidth: 1,
+    //     flexDirection: 'row',  
+    //     alignSelf: 'center',
+    //     margin: 10,
+    //     paddingHorizontal: 20,
+    //     paddingVertical: 10,
+    //     alignItems: 'center',
+    // }, 
 
     addbox: {
         width: 200,
         height: 100,
+        alignSelf: 'center',
+        borderColor: 'rgb(123, 165, 185)',
+        borderWidth: 2,
+        borderRadius: 5,
+        borderStyle: 'dashed',
         backgroundColor: 'transparent',
         margin: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
         alignItems: 'center',
         justifyContent: 'center',
     },
@@ -60,8 +141,8 @@ const styles = StyleSheet.create({
     },
 
     avatar: {
-        width: 100,
-        height: 100,
+        width: 90,
+        height: 90,
         marginRight: 15
     },
 
