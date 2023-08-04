8bdca361da1ea5fb04d8e5254eb2be6ef0d7be9f
An Ba <ngwin.ab@gmail.com>
2023-08-04 14:43:44 -0400

    display a kid's charts with data fetched from API

--- screens/ChildData.js
+++ screens/ChildData.js
@@ -1,66 +1,135 @@
-import React from "react";
-import { ScrollView, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import {
+    FlatList,
+    SectionList,
+    View,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    Button
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const ChildData = ({ navigation }) => {
-    return (
-        <ScrollView style={styles.container}>
-            <Text style={styles.heading}>Child's name</Text>
-            <TouchableOpacity style={styles.dataBox}>
-                <Text style={styles.dateLabel}>Date - Setting</Text>
-                <Separator />
-                <Text style={styles.subLabel}>Antecedent</Text>
-                <Text style={styles.subLabel}>Behavior</Text>
-                <Text style={styles.subLabel}>Consequence</Text>
-            </TouchableOpacity>
-            <Separator />
-            <TouchableOpacity style={{ display: 'flex', alignItems: 'center' }}
-                onPress={() => navigation.navigate('Record')}>
-                <Ionicons name='add-circle' size={90} color='rgb(86, 136, 159)' />
-                <Text style={styles.heading}>Add new ABC chart</Text>
-            </TouchableOpacity>
-        </ScrollView>
+const ChildData = ({ route, navigation }) => {
+    const { kidId } = route.params;
+    const [selectedId, setSelectedId] = useState();
+    const [name, setName] = useState('');
+    const [chartsRecorded, setChartsRecorded] = useState([]);
+
+    const getData = async () => {
+        try {
+            const url = `http://10.0.0.136:3000/kids/${kidId}`;
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            });
+            const data = await response.json();
+            setChartsRecorded(data.chartsRecorded);
+            setName(data.name);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        };
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const Item = ({ item, onPress }) => (
+        <TouchableOpacity onPress={onPress} style={[styles.item]}>
+            <Text style={[styles.title]}>SETTING</Text>
+            <Text style={[styles.innerText]}>{item.setting}</Text>
+            <Text style={[styles.title]}>ANTECEDENT</Text>
+            <Text style={[styles.innerText]}>{item.antecedent}</Text>
+            <Text style={[styles.title]}>BEHAVIOR</Text>
+            <Text style={[styles.innerText]}>{item.behavior}</Text>
+            <Text style={[styles.title]}>CONSEQUENCE</Text>
+            <Text style={[styles.innerText]}>{item.consequence}</Text>
+        </TouchableOpacity>
     );
-}
+
+    const renderItem = ({ item }) => {
+        return (
+            <Item
+                item={item}
+                key={item.id}
+                onPress={() => setSelectedId(item.id)}
+            />
+        );
+    };
+
+    if (chartsRecorded.length === 0) {
+        return (
+            <View style={styles.container}>
+                <Text style={{marginTop: 20}}>Let's record the first ABC chart for {name}!</Text>
+            </View>
+        );
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={chartsRecorded}
+                renderItem={renderItem}
+                keyExtractor={chartsRecorded => chartsRecorded.id}
+                extraData={selectedId}
+            />
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 10 }}>
+                <Button
+                    color='rgb(86, 136, 159)'
+                    title='Record'
+                    onPress={() => navigation.navigate('Record')}
+                />
+                <Button
+                    color='rgb(86, 136, 159)'
+                    title='Export'
+                    onPress={() => navigation.navigate('Record')}
+                />
+            </View>
+        </SafeAreaView>
+    );
+};
 
 export default ChildData;
-
-const Separator = () => <View style={styles.separator} />;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'rgb(235, 243, 250)',
-        // alignItems: 'center',
-        // justifyContent: 'center',
+    },
+
+    item: {
+        backgroundColor: 'rgb(196, 216, 228)',
+        borderColor: 'white',
+        borderRadius: 5,
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 15,
+    },
+
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#061464'
+    },
+
+    innerText: {
+        marginBottom: 10,
+        fontSize: 15
     },
 
     heading: {
+        borderWidth: 1,
+        borderColor: 'rgb(86, 136, 159)',
+        paddingHorizontal: 10,
         fontWeight: 'bold',
         fontSize: 25,
         color: '#061464',
-        margin: 10,
+        marginLeft: 15,
     },
-
-    dateLabel: {
-        fontSize: 20,
-        color: '#061464'
-    },
-
-    subLabel: {
-        fontSize: 18,
-        color: '#061464'
-    },
-
-    dataBox: {
-        backgroundColor: 'rgb(167, 200, 215)',
-        marginTop: 20,
-        marginHorizontal: 15,
-        padding: 20,
-    },
-
-    separator: {
-        marginVertical: 10,
-    },
-
 });
+
+
