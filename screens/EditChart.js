0cdd0080e45337e31ff30e468f26c727589b280a
An Ba <ngwin.ab@gmail.com>
2023-08-07 01:45:49 -0400

    add Delete button on Edit chart screen

--- screens/EditChart.js
+++ screens/EditChart.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { StyleSheet, Text, View, Button, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { StackActions } from '@react-navigation/native';
 import CalendarBox from '../components/base_components/CalendarBox';
@@ -25,8 +24,8 @@ const InputBox = ({ label, value, onChangeText }) => {
 const EditChart = ({ route, navigation }) => {
     const { chart } = route.params;
     const { kidName } = route.params;
-
     const [data, setData] = useState([]);
+
     const [formValues, setFormValues] = useState({
         setting: '',
         antecedent: '',
@@ -34,25 +33,7 @@ const EditChart = ({ route, navigation }) => {
         consequence: '',
     });
 
-    // const getData = () => {
-    //     const url = `http://10.0.0.136:3000/kids/${kidId}`;
-    //     fetch(url, {
-    //         method: 'GET',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //     })
-    //         .then((response) => response.json())
-    //         .then((result) => {
-    //             setData(result);
-    //         })
-    //         .catch((error) => {
-    //             console.error('Error fetching data:', error);
-    //         });
-    // };
-
     useEffect(() => {
-        //getData();
         setFormValues({
             setting: chart.setting,
             antecedent: chart.antecedent,
@@ -72,49 +53,89 @@ const EditChart = ({ route, navigation }) => {
                 body: JSON.stringify(formValues),
             });
             const data = await response.json();
-            
+
         } catch (error) {
             console.error('Error saving data:', error);
         }
     };
 
+    const deleteChart = async () => {
+        try {
+            const url = `http://10.0.0.136:3000/charts/${chart._id}`;
+            const response = await fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (response.ok) {
+                Alert.alert('The chart has been deleted.');
+                navigation.dispatch(StackActions.pop(1));
+            } else {
+                throw new Error('Failed to delete the chart.');
+            }
+        } catch (error) {
+            console.error('Error deleting chart:', error);
+        }
+    };
+
+    const handleDeleteChart = () => {
+        Alert.alert('Are you sure you want to delete this chart?', null, [
+            {
+                text: 'No',
+                style: 'cancel',
+            },
+            {
+                text: 'Yes',
+                onPress: () => deleteChart(),
+            },
+        ]);
+    };
+
     return (
-        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ minHeight: '100%' }}>
-            <View style={styles.container}>
-                <Text style={styles.heading}>{kidName}</Text>
-                <Separator />
-                <InputBox
-                    label="Setting"
-                    onChangeText={(text) => setFormValues({ ...formValues, setting: text })}
-                    value={formValues.setting}
+
+        <View style={styles.container}>
+            <Text style={styles.heading}>{kidName}</Text>
+            <Separator />
+            <InputBox
+                label="Setting"
+                onChangeText={(text) => setFormValues({ ...formValues, setting: text })}
+                value={formValues.setting}
+            />
+            <InputBox
+                label="Antecedent"
+                onChangeText={(text) => setFormValues({ ...formValues, antecedent: text })}
+                value={formValues.antecedent}
+            />
+            <InputBox
+                label="Behavior"
+                onChangeText={(text) => setFormValues({ ...formValues, behavior: text })}
+                value={formValues.behavior}
+            />
+            <InputBox
+                label="Consequence"
+                onChangeText={(text) => setFormValues({ ...formValues, consequence: text })}
+                value={formValues.consequence}
+            />
+
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 10 }}>
+                <Button
+                    color='rgb(86, 136, 159)'
+                    title='Save Chart'
+                    onPress={() => {
+                        saveData();
+                        navigation.dispatch(StackActions.pop(1));
+                    }}
                 />
-                <InputBox
-                    label="Antecedent"
-                    onChangeText={(text) => setFormValues({ ...formValues, antecedent: text })}
-                    value={formValues.antecedent}
+                <Button
+                    color='rgb(86, 136, 159)'
+                    title='Delete Chart'
+                    onPress={() => handleDeleteChart()}
                 />
-                <InputBox
-                    label="Behavior"
-                    onChangeText={(text) => setFormValues({ ...formValues, behavior: text })}
-                    value={formValues.behavior}
-                />
-                <InputBox
-                    label="Consequence"
-                    onChangeText={(text) => setFormValues({ ...formValues, consequence: text })}
-                    value={formValues.consequence}
-                />
-                <View style={{ display: 'flex', flexDirection: 'row', alignSelf: 'center', marginTop: 20 }}>
-                    <Button
-                        title="Save"
-                        color="rgb(86, 136, 159)"
-                        onPress={() => {
-                            saveData();
-                            navigation.dispatch(StackActions.pop(1));
-                        }}
-                    />
-                </View>
             </View>
-        </ScrollView>
+
+        </View>
+
     );
 };
 
