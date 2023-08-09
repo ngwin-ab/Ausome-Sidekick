bcad7edfcf3c0f95a09f18171766c5e4473b253d
An Ba <ngwin.ab@gmail.com>
2023-08-09 05:57:56 -0400

    Draft content for Settings screen

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, Button, StyleSheet } from 'react-native';
 
 const Settings = ({ navigation }) => {
     return (
-        <View style={styles.container}>
-            <Text>Settings Screen</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <Text style={styles.heading}>Account</Text>
+            <Text style={styles.heading}>Reminders</Text>
+            <Text style={styles.heading}>Invite family</Text>
+            <Text style={styles.heading}>Language</Text>
+            <View style={{ display: 'flex', flexDirection: 'row', alignSelf: 'center' }}>
+                    <Button
+                        title='Log Out'
+                        color='rgb(96, 147, 171)'
+                        onPress={() => {}}
+                    />
+            </View>
+        </ScrollView>
     );
 }
 
@@ -15,7 +25,17 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'rgb(235, 243, 250)',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+
+    heading: {
+        fontWeight: 'bold',
+        fontSize: 25,
+        color: '#3c5e6e',
+        margin: 10,
+        borderWidth: 1,
+        borderRadius: 10,
+        padding: 10
     },
 });
