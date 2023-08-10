76603d78051bd25790176fee125386db33ae8d40
An Ba <ngwin.ab@gmail.com>
2023-08-10 03:17:28 -0400

    Revert "Create Chart component and reuse in ChartList"
    This reverts commit f4a0817962c2495d041b037acaf3624b173687ae.

--- components/ChartList.js
+++ components/ChartList.js
@@ -9,16 +9,29 @@ import {
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useIsFocused } from '@react-navigation/native';
-import Chart from './Chart';
 
 const ChartList = ({ navigation, chartsRecorded, name, kidId }) => {
     const isFocused = useIsFocused();
     const [selectedId, setSelectedId] = useState('');
 
+    const Item = ({ item, onPress }) => (
+        <TouchableOpacity onPress={onPress} style={[styles.item]}>
+            <Text style={[styles.title]}>SETTING</Text>
+            <Text style={[styles.innerText]}>{item.setting}</Text>
+            <Text style={[styles.title]}>ANTECEDENT</Text>
+            <Text style={[styles.innerText]}>{item.antecedent}</Text>
+            <Text style={[styles.title]}>BEHAVIOR</Text>
+            <Text style={[styles.innerText]}>{item.behavior}</Text>
+            <Text style={[styles.title]}>CONSEQUENCE</Text>
+            <Text style={[styles.innerText]}>{item.consequence}</Text>
+        </TouchableOpacity>
+    );
+
     const renderItem = ({ item }) => {
         return (
-            <Chart
+            <Item
                 item={item}
+                key={item.id}
                 onPress={() => {
                     setSelectedId(item.id);
                     navigation.navigate('EditChart', { chart: item, kidName: name });
@@ -79,6 +92,27 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+
+    item: {
+        backgroundColor: 'rgb(196, 216, 228)',
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 5,
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 15,
+    },
+
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#3c5e6e'
+    },
+
+    innerText: {
+        marginBottom: 10,
+        fontSize: 15
     },
 });
 
