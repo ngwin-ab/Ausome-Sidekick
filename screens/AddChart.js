d5a709c4623ebbc79822cb8d046dd7bc55d63e66
An Ba <ngwin.ab@gmail.com>
2023-08-10 03:23:01 -0400

    Revert "Separate NewChartForm  from AddChart screen"
    This reverts commit 38af0ad850c9967036cfa3360b536c599f9a4b8b.

--- screens/AddChart.js
+++ screens/AddChart.js
@@ -3,7 +3,23 @@ import { useEffect } from 'react';
 import { StyleSheet, Text, View, Button, ScrollView, TouchableOpacity } from 'react-native';
 import { TextInput } from 'react-native-paper';
 import { StackActions } from '@react-navigation/native';
-import NewChartForm from '../components/NewChartForm';
+import CalendarBox from '../components/base_components/CalendarBox';
+
+const Separator = () => <View style={styles.separator} />;
+const InputBox = ({ label, value, onChangeText }) => {
+    return (
+        <View>
+            <TextInput
+                style={{ height: 78, marginHorizontal: 15, marginVertical: 5 }}
+                mode="outlined"
+                multiline
+                label={label}
+                value={value}
+                onChangeText={onChangeText}
+            />
+        </View>
+    );
+};
 
 const AddChart = ({ route, navigation }) => {
     const { kidId } = route.params;
@@ -63,8 +79,42 @@ const AddChart = ({ route, navigation }) => {
             <Text>Loading...</Text>
         </View>
     ) : (
-                <NewChartForm data={data} saveData={saveData} navigation={navigation} />
-        )
+        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ minHeight: '100%' }}>
+            <View style={styles.container}>
+                <Text style={styles.heading}>{data.name}</Text>
+                <Separator />
+                <InputBox
+                    label="Setting"
+                    onChangeText={(text) => setFormValues({ ...formValues, setting: text })}
+                    value={formValues.setting}
+                />
+                <InputBox
+                    label="Antecedent"
+                    onChangeText={(text) => setFormValues({ ...formValues, antecedent: text })}
+                    value={formValues.antecedent}
+                />
+                <InputBox
+                    label="Behavior"
+                    onChangeText={(text) => setFormValues({ ...formValues, behavior: text })}
+                    value={formValues.behavior}
+                />
+                <InputBox
+                    label="Consequence"
+                    onChangeText={(text) => setFormValues({ ...formValues, consequence: text })}
+                    value={formValues.consequence}
+                />
+                <View style={{ display: 'flex', flexDirection: 'row', alignSelf: 'center', marginTop: 20 }}>
+                    <Button
+                        title="Save"
+                        color="rgb(96, 147, 171)"
+                        onPress={() => {
+                            saveData();
+                            navigation.dispatch(StackActions.pop(1));
+                        }}
+                    />
+                </View>
+            </View>
+        </ScrollView>)
     )
 };
 
@@ -74,6 +124,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'rgb(235, 243, 250)',
+        // alignItems: 'stretch',
+        // justifyContent: 'center',
+    },
+
+    separator: {
+        marginVertical: 10,
     },
 
     heading: {
--- screens/EditChart.js
+++ screens/EditChart.js
@@ -145,6 +145,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'rgb(235, 243, 250)',
+        // alignItems: 'stretch',
+        // justifyContent: 'center',
     },
 
     separator: {
