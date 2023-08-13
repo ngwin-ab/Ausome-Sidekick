62509d71bd3e6cb196ff37d65fe916395cbaf8d3
An Ba <ngwin.ab@gmail.com>
2023-08-13 00:17:39 -0400

    Styling

--- screens/About.js
+++ screens/About.js
@@ -4,13 +4,19 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 const About = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>The Ausome Sidekick app was created as a tool for parents and teachers
-                to record ABC charts and, in turn, better understand the function of children's behaviors. </Text>
+            <Text style={styles.intro}>Ausome Sidekick was developed as a tool for parents and teachers, particularly those with autistic children
+                to record ABC charts and, as a result better understand the function of children's behaviors.{'\n'}
+                In the long run, it is always recommended to seek for help from a qualified professional.</Text>
             <Text style={styles.heading}>Attribution</Text>
-            <Text style={styles.text}>App Logo:</Text>
-            <Text style={styles.text}>Designed by catalyststuff / Freepik.com</Text>
-            <Text style={styles.text}>Avatar icons:</Text>
-            <Text style={styles.text}>Designed by 千图网 / Pngtree.com</Text>
+            <View style={styles.attribution}>
+                <Text style={{fontSize: 18}}>App logo:</Text>
+                <Text style={{fontSize: 18}}>Designed by catalyststuff / Freepik.com</Text>
+            </View>
+
+            <View style={styles.attribution}>
+                <Text style={{fontSize: 18}}>Avatar icons:</Text>
+                <Text style={{fontSize: 18}}>Designed by 千图网 / Pngtree.com</Text>
+            </View>
         </View>
     );
 }
@@ -23,18 +29,25 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgb(235, 243, 250)',
     },
 
-    text: {
-        margin: 15,
-        fontSize: 18
+    intro: {
+        marginVertical: 20,
+        marginHorizontal: 20,
+        fontSize: 18,
+        textAlign: 'auto',
+        lineHeight: 20,
+    },
+
+    attribution: {
+        marginHorizontal: 40,
+        textAlign: 'auto',
+        lineHeight: 30,
     },
 
     heading: {
         fontWeight: 'bold',
-        fontSize: 25,
+        fontSize: 22,
         color: '#445E92',
-        margin: 10,
-        borderWidth: 1,
-        borderRadius: 10,
-        padding: 10
+        marginHorizontal: 20,
+        lineHeight: 30,
     },
 });
